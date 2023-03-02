8c088c7018fc3992dd2e919dc4be02bb561a3598
Abhimanyu-Modi <veerabhimanyu100@gmail.com>
2023-03-02 22:27:32 +0530

    Even_Odd program

---
+++ Even_Odd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Even_Odd
+{
+    public static void main(String[] args)
+    {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter number ");
+	int n=sc.nextInt();
+	boolean rs=isEvenOdd(n);
+	if(rs)
+	   System.out.println("Even");
+	else
+	   System.out.println("Odd");
+    }
+}
